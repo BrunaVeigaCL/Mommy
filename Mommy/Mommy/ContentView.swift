49340cd90526cf0dbf38258c82d4a0e9431f72ee
Bruna Veiga Chalegre de Lira <brunaveigachalegre2@gmail.com>
2024-05-13 17:21:09 -0300

    Alterando o gitignore

---
+++ Mommy/Mommy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Mommy
+//
+//  Created by Bruna Veiga Chalegre de Lira on 13/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Mommy/Mommy/MommyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MommyApp.swift
+//  Mommy
+//
+//  Created by Bruna Veiga Chalegre de Lira on 13/05/24.
+//
+
+import SwiftUI
+
+@main
+struct MommyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
