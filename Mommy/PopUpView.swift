efc908618bcd0e6cb2093dceff06ff9b673fb592
Bruna Veiga Chalegre de Lira <brunaveigachalegre2@gmail.com>
2024-05-14 17:36:32 -0300

    Adiciona as cores nos assets, cria a view do popup, e o arquivo style guide de tipografia

---
+++ Mommy/PopUpView.swift
@@ -0,0 +1,8 @@
+//
+//  PopUpView.swift
+//  Mommy
+//
+//  Created by Bruna Veiga Chalegre de Lira on 14/05/24.
+//
+
+import Foundation
---
+++ Mommy/StyleGuide.swift
@@ -0,0 +1,8 @@
+//
+//  StyleGuide.swift
+//  Mommy
+//
+//  Created by Bruna Veiga Chalegre de Lira on 14/05/24.
+//
+
+import Foundation
